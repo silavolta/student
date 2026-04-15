0f497f70d6718ec8f507a71e2eec00992eca0b67
Andrei <108016092+silavolta@users.noreply.github.com>
2026-04-15 22:12:51 +0400

    Delete C#/MauiRaider/MauiRaider directory
    Delete C#/MauiRaider/MauiRaider directory

--- C#/MauiRaider/MauiRaider/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiRaider;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- C#/MauiRaider/MauiRaider/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiRaider;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
